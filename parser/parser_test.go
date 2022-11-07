136f5b9a50fd275221479eddba1b7c94be265754
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-11-07 10:16:10 +0800

    block expression should parse multiple statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -499,3 +499,23 @@ func Test_parseIfExpression(t *testing.T) {
 		t.Errorf("exp.Alternative.Statements was not nil. got=%+v", exp.Else)
 	}
 }
+
+func Test_parseBlockExpression(t *testing.T) {
+	input := `{ let x=6;let y=42;x=x+y;y=2*x;return x<y; }`
+	l := lexer.New(input)
+	p := New(&l)
+	program, _ := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+			1, len(program.Statements))
+	}
+	block, ok := (program.Statements[0]).(ast.BlockStatement)
+	fmt.Println(block)
+	if !ok {
+		t.Fatalf("expecting ast.BlockStatement, got %T", block)
+	}
+	if len(block.Statements) != 5 {
+		t.Fatalf("expecting 5 statements, got %v", len(block.Statements))
+	}
+}
