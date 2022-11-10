5b5d5a9adfc96792f6f5c1c653a14f572a922ff6
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-11-10 21:38:13 +0800

    setup FunctionLiteral ast node

---
+++ ast/function_literal.go
@@ -0,0 +1,36 @@
+package ast
+
+import (
+	"bytes"
+	"interpreter/token"
+	"strings"
+)
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []Identifier
+	Body       BlockStatement
+}
+
+func (f FunctionLiteral) TokenLiteral() string {
+	return f.Token.Literal
+}
+
+func (f FunctionLiteral) String() string {
+	var out bytes.Buffer
+	var params []string
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString(f.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(f.Body.String())
+	return out.String()
+}
+
+func (f FunctionLiteral) expression() {
+	//TODO implement me
+	panic("implement me")
+}
